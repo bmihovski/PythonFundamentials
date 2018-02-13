073da30514d4a654e5cf4223fb8ac600045814b1
bmihovski <bmihovski@live.com>
2018-02-13 23:05:15 +0200

    week six first tested

---
+++ odd_lines.py
@@ -0,0 +1,28 @@
+"""
+Write a program that reads a text file and writes its every odd line in another file. Line numbers starts from 0.
+
+Examples
+
+Input.txt
+Two house holds, both a like in dignity,
+In fair Verona, where we lay our scene,
+From ancient grudge break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-cross'd lovers take their life;
+Whose miss adventured piteous overthrows
+Do with their death bury their parent's strife.
+
+Output.txt
+In fair Verona, where we lay our scene,
+Where civil blood makes civil hands unclean.
+A pair of star-cross'd lovers take their life;
+Do with their death bury their parent's strife.
+"""
+FILE_PATH = "./Resources/01. Odd Lines/"
+
+lines = list()
+with open(FILE_PATH + 'Input.txt') as input_file:
+    lines = (input_file.readlines())
+    with open(FILE_PATH + 'Output.txt', 'w') as out_file:
+        out_file.writelines([line for index, line in enumerate(lines) if index % 2 != 0])
